0692edac83a3db92933dc0662003ed7fb0614f88
FelineJTD <13520050@std.stei.itb.ac.id>
2024-05-07 11:50:33 +0200

    feat: add idle animation

--- src/components/canvas/Object.jsx
+++ src/components/canvas/Object.jsx
@@ -2,14 +2,28 @@
 
 import { useLoader } from '@react-three/fiber'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader"
+import { useFrame } from '@react-three/fiber'
+import { useMemo, useRef, useState } from 'react'
 
 export function Archi(props) {
+  const mesh = useRef(null)
+
+  useFrame((state, delta) => {
+    const t = state.clock.getElapsedTime()
+    mesh.current.rotation.y = Math.sin(t) * (Math.PI / 30)
+    mesh.current.rotation.x = Math.cos(t) * (Math.PI / 50)
+  })
+
   const materials = useLoader(MTLLoader, "model.mtl");
   const obj = useLoader(OBJLoader, "model.obj", (loader) => {
     materials.preload();
     loader.setMaterials(materials);
   });
 
-  return <primitive object={obj} {...props} />
+  return (
+    <group ref={mesh}>
+      <primitive object={obj} {...props} />
+    </group>
+  )
 }
